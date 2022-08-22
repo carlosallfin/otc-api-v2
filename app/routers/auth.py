a8e4e8778c7964cef8e97a2e05f78faf941a4625
Carlos Zuloaga <carlos@allfintec.com>
2022-08-22 15:37:07 -0400

    auth and schema

--- app/routers/auth.py
+++ app/routers/auth.py
@@ -19,24 +19,24 @@ def login(user_credentials: OAuth2PasswordRequestForm=Depends() ,db: Session=Dep
         if user.active==False:
             detail= 'User is inactive'
             logged=False
+        else:
+            if not utils.verify(user_credentials.password, user.password):
+                detail= 'Invalid password'
+                logged=False
 
-    if not utils.verify(user_credentials.password, user.password):
-        detail= 'Invalid password'
-        logged=False
-
-    else:
-        logged=True
-        access_token= oauth2.create_access_token(data={"user_id": user.id})
-        detail='Success'
-        token_type='Bearer'
-        current_user={
-            "id":user.id,
-            "name":user.name,
-            "email":user.email,
-            "phone":user.phone,
-            "role_id":user.role_id,
-            "active":user.active
-        }
+            else:
+                logged=True
+                access_token= oauth2.create_access_token(data={"user_id": user.id})
+                detail='Success'
+                token_type='Bearer'
+                current_user={
+                    "id":user.id,
+                    "name":user.name,
+                    "email":user.email,
+                    "phone":user.phone,
+                    "role_id":user.role_id,
+                    "active":user.active
+                }
 
     if logged==False:
         access_token=''
