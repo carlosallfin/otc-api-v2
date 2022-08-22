a4ce59493137ed97c64fce02594506af8f7bbcd9
Carlos Zuloaga <carlos@allfintec.com>
2022-08-22 15:15:04 -0400

    Modified auth response

--- app/routers/auth.py
+++ app/routers/auth.py
@@ -12,21 +12,32 @@ def login(user_credentials: OAuth2PasswordRequestForm=Depends() ,db: Session=Dep
     user = db.query(models.User).filter(models.User.email == user_credentials.username).first()
 
     if not user:
-        raise HTTPException(status_code=status.HTTP_403_FORBIDDEN, detail="Invalid Credentials")
+        detail='User does not exist'
+        logged=False
+
+    if user.active==False:
+        detail= 'User is inactive'
+        logged=False
 
     if not utils.verify(user_credentials.password, user.password):
-        raise HTTPException(status_code=status.HTTP_403_FORBIDDEN, detail="Invalid Credentials")
+        detail= 'Invalid password'
+        logged=False
 
-    access_token= oauth2.create_access_token(data={"user_id": user.id})
+    if logged==False:
+        access_token=''
+        current_user={}
 
-    current_user={
-        "id":user.id,
-        "name":user.name,
-        "email":user.email,
-        "phone":user.phone,
-        "role_id":user.role_id,
-        "active":user.active
-    }
+    else:
+        access_token= oauth2.create_access_token(data={"user_id": user.id})
+        detail='Success'
+        current_user={
+            "id":user.id,
+            "name":user.name,
+            "email":user.email,
+            "phone":user.phone,
+            "role_id":user.role_id,
+            "active":user.active
+        }
 
     
-    return {"access_token":access_token, "token_type":"Bearer", "user_data":current_user}
+    return {'logged':logged,detail:detail,"access_token":access_token, "token_type":"Bearer", "user_data":current_user}
